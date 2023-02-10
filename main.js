e19dd4e1ff99d41db57eaf537fb0d5ba8f16e250
Dhruv Patel <dhruv225@outlook.com>
2023-02-10 15:02:40 -0500

    addMark and updateBoard methods

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 const Player = function(name, type) {
     this.name = name;
     this.type = type;
@@ -14,11 +15,80 @@ const GameBoard = (() => {
     ];
     
     return {board};
-});
+})();
 
-const Display = (() => {
+
+const TicTacToe = (() => {
     let playerX = Player("Player X", "X");
-    let playerY = Player("Player Y", "Y");
+    let playerY = Player("Player O", "O");
+    const cells = document.querySelectorAll('.cell');
+    const gameMessage = document.querySelector('.gameMessage');
+    let playerTurn = playerX.type;
 
+    const toggleTurn = function() {
+        if (playerTurn === playerX.type) {
+            playerTurn = playerY.type
+        }
+        else if (playerTurn === playerY.type) {
+            playerTurn = playerX.type;
+        }
+    }
+
+    //add to board
+    const addMark = function(e) {
+        if (e.target.textContent === ""){
+            if (playerTurn === "O"){
+                e.target.style.color = "red";
+            }
+            else {
+                e.target.style.color = "blue";
+            }
+            e.target.textContent = playerTurn;
+        }
+        console.log("fd");
+        toggleTurn();
+        updateBoard();
+    }
     
-})
+
+    //check board for winner
+
+
+    // update board
+    const updateBoard = function() {
+        let i = 0;
+        let j = 0;
+        cells.forEach(cell => {
+            if (j>2){
+                j = 0;
+                i++;
+            }
+            GameBoard.board[i][j] = cell.textContent;
+            j++;
+        })
+    }
+
+    // set board
+    const setBoard = function() {
+        let i = 0;
+        let j = 0;
+        console.log(cells);
+        cells.forEach(cell => {
+            if (j>2){
+                j = 0;
+                i++;
+            }
+            GameBoard.board[i][j] = "";
+            j++;
+            console.log(i + " " + j);
+            cell.addEventListener('click', addMark);
+        });
+    }
+
+    // start game
+    // const startGame = function() {
+    // }
+    return {setBoard};
+})();
+
+TicTacToe.setBoard();
